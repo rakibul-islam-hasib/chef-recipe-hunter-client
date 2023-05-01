07f928459a5975d8ee3033dad1f59d8c1bcce2cf
Hasib Hossain <boraborhasib@gmail.com>
2023-05-01 22:15:57 +0600

    Home Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import NavBar from './components/headers/NavBar';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>hello from home</h1>
-    </div>
+    <>
+      <div className="bg-primary">
+        <NavBar />
+      </div>
+      <Outlet />
+    </>
   );
 };
 
--- src/components/headers/NavBar.jsx
+++ src/components/headers/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const navbarData = [
+    { label: "Home", url: "/" },
+    { label: "Recipes", url: "/recipes" },
+    { label: "Blog", url: "/blog" },
+    { label: "About", url: "/about" },
+    { label: "Contact", url: "/contact" }
+];
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className='w-[90%] flex justify-between text-white items-center py-4 mx-auto'>
+            <div className="">
+                <h1 className='text-4xl font-bold' >Amber Chefs</h1>
+            </div>
+            <div className="">
+                <ul className='flex justify-between gap-4'>
+                    {navbarData.map((item, index) => <li className='font-bold'><NavLink to={item.url}>{item.label}</NavLink></li>)}
+                </ul>
+            </div>
         </div>
     );
 };
